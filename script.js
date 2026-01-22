9444d289a5661b69b91929064f7fca9ac14281ec
lester <lesterc013@gmail.com>
2026-01-22 21:35:14 +0800

    Populate numRows * numCols number of cells

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// Start small
-// Create 9 divs first.
-// try to grid them 3x3
-const numRows = 3;
-const numCols = 3;
+const numRows = 16;
+const numCols = 16;
 const totalCells = numRows * numCols;
 
+// To hold all the cells
 const containerClassName = ".container";
 const container = document.querySelector(containerClassName);
 
-// Every iteration create a new div element.
-// All of the divs will have .cell so we can manipulate its styling in css.
-// Set its ID to be what cell number it is - might be useful later on.
-// Append it as a child of container
-for (let i = 0; i < totalCells; i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  cell.id = i + 1;
-  cell.textContent = "hi";
-  container.appendChild(cell);
+// One row will be a flex div
+// The number of cols will determine how many cells within that one flex div
+
+// Then we iterate the num of rows to get the total num
+
+for (let i = 0; i < numRows; i++) {
+  // Create numRows number of divs intended to be stacked on top of each other in the container.
+  const row = document.createElement("div");
+  row.id = `row-${i + 1}`;
+  row.classList = "row";
+
+  // For each row, create the numCols number of cells and append into it
+  for (let j = 0; j < numCols; j++) {
+    const cell = document.createElement("cell");
+    cell.classList = "cell";
+    row.appendChild(cell);
+  }
+
+  container.appendChild(row);
 }
