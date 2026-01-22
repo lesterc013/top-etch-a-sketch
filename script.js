e408d597ca01defb76332cd15f7d23a571d96f35
lester <lesterc013@gmail.com>
2026-01-22 22:20:48 +0800

    Add logic to sketch on grid

--- script.js
+++ script.js
@@ -6,21 +6,53 @@ const totalCells = numRows * numCols;
 const containerClassName = ".container";
 const container = document.querySelector(containerClassName);
 
-// One row will be a flex div
-// The number of cols will determine how many cells within that one flex div
+/*
+Main colouring logic is to attach event listeners to container.
+Make use of event bubbling to then work on that element to colour.
+*/
+
+// Helper to colour a target cell based on its cellId
+const colourTargetDiv = (cellId) => {
+  const cellToColour = document.querySelector("#" + cellId);
+  // Change its background colour
+  cellToColour.style.backgroundColor = "black";
+};
+
+// This colours the first div selected and clicked down on
+const colourIfMouseDownAndOverDiv = (e) => {
+  // To prevent not-allowed cursor which comes when a mousedown followed by mousedrag
+  e.preventDefault();
+  colourTargetDiv(e.target.id);
+};
+
+// This colours a div if mouse button was held down and the pointer hovers over it.
+const colourIfMouseBtnWasDownAndHoveredOver = (e) => {
+  if (e.buttons === 1) {
+    colourTargetDiv(e.target.id);
+  }
+};
+
+container.addEventListener("mousedown", colourIfMouseDownAndOverDiv);
+container.addEventListener("mouseover", colourIfMouseBtnWasDownAndHoveredOver);
+
+// Set cell ID, then increment cellId every j iteration by 1
+let cellId = 1;
 
 // Then we iterate the num of rows to get the total num
-
 for (let i = 0; i < numRows; i++) {
   // Create numRows number of divs intended to be stacked on top of each other in the container.
   const row = document.createElement("div");
   row.id = `row-${i + 1}`;
   row.classList = "row";
+  row.draggable = false;
 
   // For each row, create the numCols number of cells and append into it
   for (let j = 0; j < numCols; j++) {
     const cell = document.createElement("cell");
     cell.classList = "cell";
+    cell.id = "cell-" + cellId;
+    cellId++;
+    cell.draggable = false;
     row.appendChild(cell);
   }
 
